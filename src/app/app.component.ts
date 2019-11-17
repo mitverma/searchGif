49f6ff54576544fdfda5f672a23a2ad9697b78df
mitverma <amitverma951994@gmail.com>
2019-11-17 13:56:10 +0530

    first commit;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'searchGif';
+  apiLinkKey = 'http://api.giphy.com/v1/gifs/search?api_key=Ej5MSXtrAw0brhA6JmV97yJASSJt2yAo&q=';
+  searchText = '';
+  emailId = '';
+  showError = false;
+  searchList: any;
+  constructor(public http: HttpClient){
+
+  }
+
+  ngOnInit() {
+    this.getSearchList('developer');
+  }
+
+  getSearchList(searchText){
+    let apiLinkToCalled = this.apiLinkKey + searchText;
+    this.http.get(apiLinkToCalled).subscribe((data: any)=>{
+      if(data && data.data.length){
+          this.searchList = data.data;
+      }
+    }, err=>{
+      console.error(err);
+    })
+  }
+
+  sendEmail(emailIdValue){
+    if(emailIdValue && emailIdValue.valid){
+      this.showError = false;
+    }else {
+      this.showError = true;
+    }
+  }
 }
